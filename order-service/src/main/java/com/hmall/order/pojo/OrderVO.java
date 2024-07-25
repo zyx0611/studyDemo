780dc4e6e09c77bccb53304c41717be0008609d3
朱毅鑫zyx <12808356+zhu-yixin-zyx@user.noreply.gitee.com>
2024-07-25 20:55:58 +0800

    fix: mapstruct依赖包引入，新建实体转换类

--- order-service/src/main/java/com/hmall/order/pojo/OrderVO.java
+++ order-service/src/main/java/com/hmall/order/pojo/OrderVO.java
@@ -2,15 +2,11 @@ package com.hmall.order.pojo;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.Data;
 
 import java.util.Date;
 import java.util.List;
 
-@Data
-@TableName("tb_order")
-public class Order{
+public class OrderVO {
     /**
      * 订单编号
      */
--- order-service/src/main/java/com/hmall/order/service/impl/TaskContext.java
+++ order-service/src/main/java/com/hmall/order/service/impl/TaskContext.java
@@ -4,6 +4,9 @@ import com.hmall.order.service.BaseTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 @Service
 public class TaskContext {
@@ -11,6 +14,13 @@ public class TaskContext {
     Map<String, BaseTaskService> BaseTaskServiceMap;
 
     public BaseTaskService getTaskService(String type) {
+        // 日期格式转换类
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+        String format = simpleDateFormat.format(date);
+        // 数字格式转换
+        DecimalFormat decimalFormat = new DecimalFormat("#.00");// 保留两位小数
+        String format1 = decimalFormat.format(1.222);
         return BaseTaskServiceMap.get(type);
     }
 }
